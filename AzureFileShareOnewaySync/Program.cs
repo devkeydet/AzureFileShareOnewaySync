7b9b2c771b599173ad5180fe7e183b7a0ef22e58
devkeydet <devkeydet@gmail.com>
2017-04-16 19:29:05 -0400

    v2

--- AzureFileShareOnewaySync/Program.cs
+++ AzureFileShareOnewaySync/Program.cs
@@ -1,26 +1,35 @@
 ﻿using System;
-using System.Net;
-using Microsoft.WindowsAzure.Storage.DataMovement;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.File;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.DataMovement;
+using Microsoft.WindowsAzure.Storage.File;
 
 namespace AzureFileShareOnewaySync
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            //TODO: Identify more opportunities for parallelism and async calls and refactor accordingly.
-                        
             //Increase .NET HTP connections limit per https://github.com/Azure/azure-storage-net-data-movement#best-practice
             ServicePointManager.DefaultConnectionLimit = Environment.ProcessorCount * 8;
 
             // Turn off 100 -continue per https://github.com/Azure/azure-storage-net-data-movement#best-practice
             ServicePointManager.Expect100Continue = false;
 
+            var (sourceStorageAccount, sourceDir, destStorageAccount, destDir) = InitiliazeFromConfig();
+
+            CopyNewerSourceFilesToDestination(sourceStorageAccount, sourceDir, destStorageAccount, destDir);
+
+            DeleteDestinationFilesAndDirectoriesNoLongerInSource(destDir, sourceStorageAccount);
+        }
+
+        private static (CloudStorageAccount, CloudFileDirectory, CloudStorageAccount, CloudFileDirectory)
+            InitiliazeFromConfig()
+        {
             var sourceStorageConnectionString = ConfigurationManager.AppSettings["SourceConnectionString"];
             var sourceStorageAccount = CloudStorageAccount.Parse(sourceStorageConnectionString);
             var sourceFileClient = sourceStorageAccount.CreateCloudFileClient();
@@ -33,44 +42,42 @@ namespace AzureFileShareOnewaySync
             var destShare = destFileClient.GetShareReference(ConfigurationManager.AppSettings["DestinationShare"]);
             var destDir = destShare.GetRootDirectoryReference();
 
-            CopyNewerSourceFilesToDestination(sourceStorageAccount, sourceDir, destStorageAccount, destDir);
-
-            DeleteDestinationFilesAndDirectoriesNoLongerInSource(destDir, sourceStorageAccount);
+            return (sourceStorageAccount, sourceDir, destStorageAccount, destDir);
         }
 
-        private static TransferStatus CopyNewerSourceFilesToDestination(CloudStorageAccount sourceStorageAccount, CloudFileDirectory sourceDir, CloudStorageAccount destStorageAccount, CloudFileDirectory destDir)
+        private static void CopyNewerSourceFilesToDestination(
+            CloudStorageAccount sourceStorageAccount,
+            CloudFileDirectory sourceDir,
+            CloudStorageAccount destStorageAccount,
+            CloudFileDirectory destDir)
         {
             var transferContext = new TransferContext();
 
-            transferContext.FileFailed += new EventHandler<TransferEventArgs>((sender, args) =>
-            {
-                Console.WriteLine(args.Exception.Message);
-            });
+            transferContext.FileFailed += (sender, args) => { Console.WriteLine(args.Exception.Message); };
 
-            transferContext.FileSkipped += new EventHandler<TransferEventArgs>((sender, args) =>
+            transferContext.FileSkipped += (sender, args) =>
             {
                 //Console.WriteLine(args.Exception.Message);
-            });
+            };
 
-            transferContext.FileTransferred += new EventHandler<TransferEventArgs>((sender, args) =>
+            transferContext.FileTransferred += (sender, args) =>
             {
                 Console.WriteLine($"Completed transferring {args.Destination}");
-            });
+            };
 
             transferContext.OverwriteCallback = (source, destination) =>
             {
                 // By default, files are not overwritten.  We want to overwrite files when the source has a newer file than the destination.
                 var sourceFile = new CloudFile(new Uri(source), sourceStorageAccount.Credentials);
-                sourceFile.FetchAttributes();
                 var destFile = new CloudFile(new Uri(destination), destStorageAccount.Credentials);
-                destFile.FetchAttributes();
 
-                if (sourceFile.Properties.LastModified > destFile.Properties.LastModified)
-                {
-                    return true;
-                }
+                // Execute in parallel
+                var sourceFileFetchAttributesTask = Task.Run(() => sourceFile.FetchAttributes());
+                var destFileFetchAttributesTask = Task.Run(() => destFile.FetchAttributes());
 
-                return false;
+                Task.WaitAll(sourceFileFetchAttributesTask, destFileFetchAttributesTask);
+
+                return sourceFile.Properties.LastModified > destFile.Properties.LastModified;
             };
 
             var options = new CopyDirectoryOptions
@@ -78,42 +85,58 @@ namespace AzureFileShareOnewaySync
                 Recursive = true
             };
 
-            var task = TransferManager.CopyDirectoryAsync(sourceDir, destDir, true, options, transferContext);
-            task.Wait();
-
-            return task.Result;
+            TransferManager.CopyDirectoryAsync(sourceDir, destDir, true, options, transferContext).Wait();
         }
 
-        private static void DeleteDestinationFilesAndDirectoriesNoLongerInSource(CloudFileDirectory destDir, CloudStorageAccount sourceStorageAccount)
+        private static void DeleteDestinationFilesAndDirectoriesNoLongerInSource(
+            CloudFileDirectory destDir,
+            CloudStorageAccount sourceStorageAccount)
         {
             var sourceEndpoint = sourceStorageAccount.FileEndpoint.ToString().TrimEnd('/');
-            var filesAndDirectories = destDir.ListFilesAndDirectories();
-            Parallel.ForEach(filesAndDirectories, (fileItem) =>
+            var filesAndDirectories = ListFilesAndDirectoriesAsync(destDir).Result;
+            Parallel.ForEach(filesAndDirectories, fileItem =>
             {
                 var cloudFile = fileItem as CloudFile;
                 if (cloudFile != null)
                 {
-                    var sourceFile = new CloudFile(new Uri($"{sourceEndpoint}{fileItem.Uri.AbsolutePath}"), sourceStorageAccount.Credentials);
-                    if (!sourceFile.Exists())
-                    {
-                        cloudFile.Delete();
-                        Console.WriteLine($"Deleted: {cloudFile.Uri}");
-                    }
+                    var sourceFile = new CloudFile(
+                        new Uri($"{sourceEndpoint}{fileItem.Uri.AbsolutePath}"),
+                        sourceStorageAccount.Credentials);
+
+                    if (sourceFile.Exists()) return;
+
+                    cloudFile.Delete();
+                    Console.WriteLine($"Deleted: {cloudFile.Uri}");
                 }
                 else
                 {
                     var cloudFileDirectory = fileItem as CloudFileDirectory;
-                    if (cloudFileDirectory != null)
-                    {
-                        DeleteDestinationFilesAndDirectoriesNoLongerInSource(cloudFileDirectory, sourceStorageAccount);
-                        if (cloudFileDirectory.ListFilesAndDirectories().Count() == 0)
-                        {
-                            cloudFileDirectory.Delete();
-                            Console.WriteLine($"Deleted directory: {cloudFileDirectory.Uri}");
-                        }
-                    }
+
+                    if (cloudFileDirectory == null) return;
+
+                    DeleteDestinationFilesAndDirectoriesNoLongerInSource(cloudFileDirectory, sourceStorageAccount);
+
+                    if (cloudFileDirectory.ListFilesAndDirectories().Count() != 0) return;
+
+                    cloudFileDirectory.Delete();
+                    Console.WriteLine($"Deleted directory: {cloudFileDirectory.Uri}");
                 }
             });
+        }
+
+        private static async Task<List<IListFileItem>> ListFilesAndDirectoriesAsync(CloudFileDirectory destDir)
+        {
+            FileContinuationToken continuationToken = null;
+            var results = new List<IListFileItem>();
+            do
+            {
+                var response = await destDir.ListFilesAndDirectoriesSegmentedAsync(continuationToken)
+                    .ConfigureAwait(false);
+                continuationToken = response.ContinuationToken;
+                results.AddRange(response.Results);
+            } while (continuationToken != null);
+
+            return results;
         }
     }
 }
